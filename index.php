c877c433cae8c3542e15d3e6b3e448bfbcfe2db1
Rob Wood <rwood3@mail.greenriver.edu>
2020-01-13 14:51:59 -0500

    added header comments

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+  * Rob Wood
+  * 1/13/2020
+  * This is a PHP Review Assignment: Cupcakes
+  * This is a webpage to fake order some cupcakes. User can input their name and click on the flavors
+  * that they want to order and will show a confirmation page of the order and the cost of the order
+  * when they submit the order.
+  */
 include("data.php")
 
 ?>
--- orderConfirmation.php
+++ orderConfirmation.php
@@ -1,3 +1,15 @@
+<?php
+
+/*
+  * Rob Wood
+  * 1/13/2020
+  * This is a PHP Review Assignment: Cupcakes
+  * This is a webpage to fake order some cupcakes. User can input their name and click on the flavors
+  * that they want to order and will show a confirmation page of the order and the cost of the order
+  * when they submit the order.
+  */
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
